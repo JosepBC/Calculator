3697e90bb66dabe1d28291501eaa4744c69002a2
Josep Bello <bellocontacto@gmail.com>
2022-01-18 16:43:57 +0100

    Interview example

--- in.jl
+++ in.jl
@@ -10,8 +10,15 @@ comment
 vector = [10;20;30]
 mtx = [10 20;20 40]
 elem = vector[0]
-elem = mtx[0, 1]
+a = 1
+elem = mtx[0, a]
 v2 = 2 * vector
-e = ten > eleven || true
+e = ten + 2 > eleven || true
 (ten + ten) >= 20
 #comment
+vector2 = vector + vector
+vector3 = vector * vector
+mtx2 = mtx * vector
+s = "hola"
+s2 = s * "hola2"
+p = (ten + ten) * 2
